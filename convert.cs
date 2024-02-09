e82430b878a6853afc10ab6f4024426308faef8a
Maria Møll <marim18@uia.no>
2024-02-09 17:46:12 +0100

    Made the initial draft with placeholders

---
+++ convert.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Formats.Asn1;
+namespace converter{
+
+class convertfunctions{
+    float InchesToXConverter(float number, String arg){
+        string cm = "cm";
+        string mm = "mm";
+        string m = "m";
+        float output = 0;
+     
+
+        if (arg ==  cm){
+          //do stuff
+         
+        }  
+         if (arg ==  mm){
+          //do stuff
+         
+        }  
+         if (arg ==  m){
+          //do stuff
+         
+        }  
+        return output;
+        
+    }
+}}
